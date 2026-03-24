3e5f3774d956eff80f90199aa51c472a8604e0dd
chstrAfk <chstrngt@gmail.com>
2026-03-24 09:09:45 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,51 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Console
+namespace ConsoleApp
 {
+    // Create a class named MathClass
+    class MathClass
+    {
+        // Create a void method that takes two integers as parameters
+        // The method performs a math operation on the first integer
+        // and displays the second integer
+        public void DoMath(int num1, int num2)
+        {
+            // Perform a math operation on the first integer (example: multiply by 2)
+            int result = num1 * 2;
+
+            // Display the result of the math operation
+            Console.WriteLine("Result of first number * 2: " + result);
+
+            // Display the second integer
+            Console.WriteLine("Second number is: " + num2);
+        }
+    }
+
+    // Main program class
     internal class Program
     {
         static void Main(string[] args)
         {
-            using System; // Allows us to use basic system functions like Console
+            // Instantiate (create an object of) the MathClass
+            MathClass mathObject = new MathClass();
 
-// Create a class named MathClass
-class MathClass
-        {
-            // Create a void method that takes two integers as parameters
-            // The method performs a math operation on the first integer
-            // and displays the second integer
-            public void DoMath(int num1, int num2)
-            {
-                // Perform a math operation on the first integer (example: multiply by 2)
-                int result = num1 * 2;
+            // Call the method and pass two numbers normally
+            mathObject.DoMath(5, 10);
 
-                // Display the result of the math operation
-                Console.WriteLine("Result of first number * 2: " + result);
+            // Call the method again, but this time specify parameters by name
+            mathObject.DoMath(num1: 7, num2: 20);
 
-                // Display the second integer
-                Console.WriteLine("Second number is: " + num2);
-            }
+            // Prevent the console from closing immediately
+            Console.ReadLine();
         }
-
-        // Main program class
-        class Program
-        {
-            static void Main(string[] args)
-            {
-                // Instantiate (create an object of) the MathClass
-                MathClass mathObject = new MathClass();
-
-                // Call the method and pass two numbers normally
-                mathObject.DoMath(5, 10);
-
-                // Call the method again, but this time specify parameters by name
-                mathObject.DoMath(num1: 7, num2: 20);
-
-                // Prevent the console from closing immediately
-                Console.ReadLine();
-            }
-        }
-    }
     }
 }
